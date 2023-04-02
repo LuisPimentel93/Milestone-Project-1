438647e07c1cb869d53b4d57540324349ecd069b
LuisPimentel93 <120436164+LuisPimentel93@users.noreply.github.com>
2023-04-02 15:25:10 -0400

    added a background opicture

---
+++ main-menu.js
@@ -0,0 +1,21 @@
+function startGame(){
+    
+}
+
+
+
+
+// function startGame() {
+// console.log("hellow world")
+// }
+ 
+// startGame()
+// start(){
+
+
+// toggleScreen(id,toggle) {
+//     let element = document.getElementById(id);
+//     let display = (toggle) ? 'block' : 'none';
+//     element.style.display = display
+// }
+// }
